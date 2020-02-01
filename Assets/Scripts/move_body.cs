50741cf265ceb5abd6443a566c55b425ae2875c1
alex <alexanderriley@comcast.net>
2020-02-01 04:31:22 -0800

    added ragdoll arm and arm snap (untested)

---
+++ Assets/Scripts/move_body.cs
@@ -0,0 +1,28 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_body : MonoBehaviour {
+
+    Rigidbody rb;
+    public float force = 5000;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetAxis("Vertical") > 0.1 || Input.GetAxis("Vertical") < -0.1) {
+            Debug.Log("FORCE");
+            rb.AddForce(new Vector3(0, Input.GetAxis("Vertical")  * force * Time.deltaTime,0));
+        }
+
+        if (Input.GetAxis("Horizontal") > 0.1 || Input.GetAxis("Horizontal") < -0.1)
+        {
+            Debug.Log("FORCE");
+            rb.AddForce(new Vector3( Input.GetAxis("Vertical") * force * Time.deltaTime, 0, 0));
+        }
+    }
+}
